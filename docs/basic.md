b338362ec81b1e1aeb141ededc41b4bbcaec397f
snowlocked <git@heixie.uu.me>
2019-11-29 16:11:32 +0800

    feat: 添加颜色

--- docs/basic.md
+++ docs/basic.md
@@ -2,7 +2,50 @@ basic usage
 
 ```vue
 <template>
-  <log-viewer log="here is some logs.\n test some logs"/>
+  <div>
+    <div style="margin-bottom:8px;">Show travis log</div>
+    <log-viewer :log="travisLog"/>
+    <div style="margin-top:16px;margin-bottom:8px;">Show serverless log</div>
+    <log-viewer :log="serverlessLog"/>
+  </div>
 </template>
+
+<script>
+const get = url=>new Promise((resolve, reject)=>{
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET",url)
+  xhr.send()
+  xhr.onload = data => resolve(data.target.responseText)
+})
+export default {
+  data(){
+    return {
+      travisLog: '',
+      serverlessLog:'',
+      devopsLog:'',
+    }
+  },
+  mounted(){
+    this.getTravisLog()
+    this.getServerlessLog()
+    this.getDevopsLog()
+  },
+  methods:{
+    getTravisLog(){
+      const travisUrl = 'https://api.travis-ci.com/v3/job/261228572/log.txt' // you can edit other travisUrl.
+      get(travisUrl).then((text)=>{
+        this.travisLog = text
+      })
+    },
+    getServerlessLog(){
+      const logUrl = 'http://39.100.141.76:3000/mock/922/log/serverless'
+      get(logUrl).then(res=>{
+        res = JSON.parse(res)
+        this.serverlessLog = res.payload.logs
+      })
+    }
+  }
+}
+</script>
 
 ```
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,40 +1,12 @@
-const ENCODED_NEWLINE = /\r{0,1}\n/
-const START_CONTROL = /\x1b\[((3[0-7])|(90)){0,1}(;{0,1}[134]){0,1}m/g
-const END_CONTROL = /\x1b\[0m/g
-const ERASE_IN_LINE_FLAG = /\x1b\[[0-2]{0,1}K/
-const POPULAR_PRIVATE_SEQUENCES = /\x1b\[\?25[hl]/g
+import ansiParse from './ansi-parse'
+
+const ENCODED_NEWLINE = /\r{0,1}\n(?!\u0008)/
 
 // RegExp reference:
 // http://jafrog.com/2013/11/23/colors-in-terminal.html
 // https://en.wikipedia.org/wiki/ANSI_escape_code
 
-const controlTags = [START_CONTROL, END_CONTROL, POPULAR_PRIVATE_SEQUENCES]
-
 export const split2Lines = str => str.split(ENCODED_NEWLINE)
-
-// 初版只做删除控制符
-export const removeControlTags = str => {
-  controlTags.forEach(tag => {
-    str = str.replace(tag, '')
-  })
-  return str
-}
-
-// 测试\x1b[[0-2]K和\x1b[K 将该行内容删除控制符
-const hasEraseInLineFlag = str => {
-  return ERASE_IN_LINE_FLAG.test(str)
-}
-
-// 执行删除该行控制符之前的内容
-export const removeEraseInLineFlag = str => {
-  const strLines = str.split(ERASE_IN_LINE_FLAG)
-  const len = strLines.length
-  // console.log(strLines);
-  if (strLines[len - 1].trim()) {
-    return strLines[len - 1]
-  }
-  return ''
-}
 
 export default log => {
   const stringLines = split2Lines(log)
@@ -43,11 +15,7 @@ export default log => {
     if (!line) {
       return
     }
-    if (hasEraseInLineFlag(line)) {
-      line = removeEraseInLineFlag(line)
-    }
-    line = removeControlTags(line)
-    stringLinesText.push(line)
+    stringLinesText.push(ansiParse(line))
   })
   return stringLinesText
 }
